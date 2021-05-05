99f0d83f555daaad2433078b0f3cd92fc862c084
khufkens <koen.hufkens@gmail.com>
2021-05-05 12:01:56 +0200

    document routines

---
+++ data/buffer_pixels.R
@@ -0,0 +1,28 @@
+# erode pixels
+library(raster)
+
+# set land cover classes
+lc <- c("DB","DN")
+
+# moving window analysis
+lapply(lc, function(cl){
+  
+  # read in data
+  r <- raster(sprintf("data/land_cover/IGBP_%s_2010_2019.tif",cl))
+  
+  # focal / moving window operation
+  r <- focal(
+    r,
+    w = matrix(1,
+               ncol = 3,
+               nrow = 3),
+    mean,
+    na.rm = FALSE)
+  
+  # write data to file
+  writeRaster(
+    r,
+    sprintf("data/land_cover/IGBP_%s_2010_2019_eroded.tif",cl),
+    options="COMPRESS=DEFLATE", 
+    overwrite=TRUE)
+})
--- data/select_random_pixels.R
+++ data/select_random_pixels.R
@@ -1,18 +1,24 @@
 # Random pixel selection
 library(raster)
-library(spatstat)
 
-# load tif file
-r <- raster("data/land_cover/IGBP_DB_2010_2019_eroded.tif")
+# set land cover classes
+lc <- c("DB","DN")
 
-# export all DB pixel locations and save as RDS
-rr <- rasterToPoints(r, fun=function(x){x==4})
-rr <- rr[,1:2]
-saveRDS(rr, "data/locations.rds")
+# process land cover classes
+lapply(lc, function(cl){
+  # load tif file
+  r <- raster(sprintf("data/land_cover/IGBP_%s_2010_2019_eroded.tif",cl))
+  
+  # export all DB pixel locations and save as RDS
+  rr <- rasterToPoints(r, fun=function(x){x==4})
+  rr <- rr[,1:2]
+  saveRDS(rr, sprintf("data/%s_locations.rds",cl))
+  
+  # pseudo random locations
+  loc <- sample(1:nrow(rr), size = 8000)
+  ss <- rr[loc,]
+  
+  # write subset to disk
+  saveRDS(ss, sprintf("data/%s_location_selection.rds",cl))
+})
 
-# pseudo random locations
-loc <- sample(1:nrow(rr), size = 8000)
-ss <- rr[loc,]
-
-# write subset to disk
-saveRDS(ss, "data/location_selection.rds")
